7c9314baeb0f438258e47847a4c5f1e37001b323
Bruno <b.spolidorio@gmail.com>
2019-11-25 18:55:21 +0000

    Modify navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,24 @@
 // An object literal
 var app = {
   init: function() {
-    app.functionOne();
+    app.changeNavBackground();
   },
-  functionOne: function () {
+  changeNavBackground: function() {
+    var scroll_start = 0;
+    var startchange = $("#startchange");
+    var offset = startchange.offset();
+    if (startchange.length) {
+      $(document).scroll(function() {
+        scroll_start = $(document).scrollTop();
+        if (scroll_start > offset.top) {
+          $("#header-nav").addClass("nav-scroll");
+        } else {
+          $("#header-nav").removeClass("nav-scroll");
+        }
+      });
+    }
   }
 };
-$("document").ready(function () {
+$("document").ready(function() {
   app.init();
 });
